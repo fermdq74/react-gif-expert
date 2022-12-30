27d3ef2044761801d0a18aafb45296a7163e005f
Fernando Petroli <fernando@tectonica.co>
2022-12-30 11:29:30 -0300

    First commit

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+/*
+import { AddCategory } from "./AddCategory";
+import { GifGrid } from "./GifGrid";
+import { GifItem } from "./GifItem";
+*/
+
+export * from './AddCategory';
+export * from './GifGrid';
+export * from './GifItem';
